2b3fd5840ef2dcaf341bb2f64727f9bfda2cdaf7
cesar00015 <cesar00015@gmail.com>
2015-11-06 20:25:24 -0500

    Primera version del programa "Factorial".

---
+++ Factorial/src/Factorial.java
@@ -0,0 +1,17 @@
+
+public class Factorial {
+
+	public static int f_Factorial(int a){
+		
+		if (a == 1)
+			return 1;
+		else
+			return a * f_Factorial(a-1);
+	}
+		
+	public static void main(String[] args) {	
+		int n = 10;
+		int result = f_Factorial(n);
+		System.out.printf("El factorial de %d es %d.",n, result);
+	}
+}
